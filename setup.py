8dc9feb052708c57cb22c3a2048aaa5c355a64e5
Yash Patel <yash.patel@itilite.com>
2019-11-13 17:42:12 +0530

    basic setup done!

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='volley',
+	version='0.1-dev',
+	description='Python Data Validation Library',
+	url='https://www.github.com/yashpatel-itilite/volley',
+	license='MIT',
+	packages=find_packages(exclude=('tests', 'tests/*', 'example', 'example/*')),
+	install_requires=[
+		'jmespath',
+	],
+	zip_safe=False,
+	test_suite='nose.collector',
+    tests_require=['nose']
+)
---
+++ tests/test_volley.py
@@ -0,0 +1,11 @@
+import unittest
+
+from volley import Form
+
+
+class TestVolley(unittest.TestCase):
+	def test_form_validation_succeds(self):
+		pass
+
+	def test_form_validation_fails(self):
+		pass
